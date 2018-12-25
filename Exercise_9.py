f768a9c523a472aaf02c320b8d5ad20f336a7bd5
Johannes <44004932+jroettenbacher@users.noreply.github.com>
2018-12-25 13:23:25 +0100

    included goal description in file

--- Exercise_9.py
+++ Exercise_9.py
@@ -1,2 +1,4 @@
 # Johannes Röttenbacher
 # created 25.12.2018
+# Goal: Calculate and plot F upward TOA, F downward BOA in dependence of optical thickness tau (C_ext),
+# single scattering albedo omega and scattering phase function represented by the asymmetry parameter g
